325cde2c942dfead9b4ede4e5196a624da851827
sampsonsimpson <NNK@Neilss-MBP.home>
2016-09-21 18:44:07 -0400

    update readme

--- garbage.R
+++ garbage.R
@@ -18,3 +18,19 @@ j <- sapply(n,length)
 j <- sort(j,decreasing=FASLE)
 
 jdat <- as.data.frame(j)
+
+
+library(dplyr)
+library(ggplot2)
+
+ts <- dc_crime_clean %>%
+  select(DateClean) %>%
+  group_by(DateClean) %>%
+  summarize(Count = n())
+
+timeplot <- ggplot(ts, aes(x=DateClean,y=Count))+
+  geom_line()+
+  geom_smooth()+
+  geom_text(aes(label = ts$Count),color='black')
+
+plot(timeplot)
--- ui.R
+++ ui.R
@@ -1,3 +1,12 @@
+#August 29th, 2016 --
+# >Make the markers individual by offense --group differently for the second chart 
+# > See if any significance in difference in time stamp vars 
+# > Add inputSelect for crime. (contingency for all selected > plot generated(no need for OFFENSE plot alone))
+# > If crime involved weapon (METHOD) then give stats on that (up or down over last 30 days, etc)
+# > add time slider input with vector to make it inclusive.  Set a button to control
+# > needed timeframes.
+#
+
 library(shiny)
 library(shinythemes)
 library(leaflet)
@@ -10,21 +19,33 @@ fluidPage(#theme = shinytheme("cerulean"),
           ),
           navbarPage("Crime DC",id='nav',
             tabPanel("Interactive Map",
-                     leafletOutput("mymap", width = '75%', height = '700px'),
+                     fluidRow(
+                      column(4,plotOutput("plotOffense",height=300)),
+                      column(4,plotOutput("plotDay",height=300)),
+                      column(4,plotOutput("plotShift",height=300))
+                     ),
+                     
+                     sliderInput("timeCntrl",
+                                 label = "Select Timeframe",
+                                 step = 1,
+                                 min = min(dc_crime_clean$Date),
+                                 max = max(dc_crime_clean$Date),
+                                 value = c(min(dc_crime_clean$Date),max(dc_crime_clean$Date))),
+                     
+                     leafletOutput("mymap", width = '100%', height = '600px'),
                      br(),
-                     absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                   draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                   width = 500, height = "auto",style = "opacity: .75",
-                                            
+                     absolutePanel(id = "controls",class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                                   top = 500, left = "auto", right = 20, bottom = "auto",
+                                   width = 330, height = "auto", style = "opacity: .75",
+                                           
                                    h2("Crime DC - Last 30 Days"),
                                    paste("Click on cluster or scroll to zoom-in, Click an individual marker for additional detail popup."),
                                    a("data source: http://opendata.dc.gov/datasets",href="http://opendata.dc.gov/datasets"),
                                    br(),
                                    a("author: neil kutty", href="http:/twitter.com/neilkutty"),
-                                   a("github",href="https://github.com/sampsonsimpson/DC_Crime_Data"),
+                                   a("github",href="https://github.com/sampsonsimpson/DC_Crime_Data")
                                      
-                                  plotOutput("plotOffense",height=300),
-                                  plotOutput("plotDay",height=300)
+                                  
                                             )
           ),
           tabPanel("Data Explorer",
